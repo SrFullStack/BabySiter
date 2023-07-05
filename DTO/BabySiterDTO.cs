9b7f5b716c76a85425343d1026aa3e42e952fe09
SrFullStack <rsgithubb@gmail.com>
2023-07-05 04:57:54 +0300

    05/07/2023

--- DTO/BabySiterDTO.cs
+++ DTO/BabySiterDTO.cs
@@ -11,24 +11,37 @@ namespace DTO
     {
 
 
+        //public int BabysiterId { get; set; }
+        //[MaxLength(30)]
+        //public string FirstName { get; set; } = null!;
+        //[MaxLength(2)]
+        //public string? LastName { get; set; }
+        //[Required]
+        //[MaxLength(10)]
+        //public string? Phone { get; set; }
+
+        //public int Age { get; set; }
+        //[EmailAddress]
+        //[Required]
+        //public string Email { get; set; } = null!;
+        //[Required]
+        //[StringLength(2, ErrorMessage = "Password length can't be more than 8")]
+        //public string Password { get; set; } = null!;
+        //public string? Description { get; set; }
         public int BabysiterId { get; set; }
-        [MaxLength(30)]
+     
         public string FirstName { get; set; } = null!;
-        [MaxLength(2)]
+      
         public string? LastName { get; set; }
-        [Required]
-        [MaxLength(10)]
+       
         public string? Phone { get; set; }
-   
+
         public int Age { get; set; }
-        [EmailAddress]
-        [Required]
+    
         public string Email { get; set; } = null!;
-        [Required]
-        [StringLength(2, ErrorMessage = "Password length can't be more than 8")]
+
         public string Password { get; set; } = null!;
         public string? Description { get; set; }
-
 
     }
 }
